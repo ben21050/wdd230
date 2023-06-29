a2b766d8dda7b4f802f42fee827c3772582316ec
ben21050 <ben21050@byui.edu>
2023-06-28 20:07:07 -0500

    fj

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8ce342ec0bef6453a2de8960f69f0afa'
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8ce342ec0bef6453a2de8960f69f0afa';
 
 fetch(apiURL)
   .then((response) => response.json())
@@ -6,8 +6,9 @@ fetch(apiURL)
     console.log(jsObject);
     document.querySelector('#current-temp').textContent = jsObject.main.temp.toFixed(1);
 
-    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const iconsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
     const desc = jsObject.weather[0].description;
+    
     document.querySelector('#icon-src').textContent = iconsrc;
     document.querySelector('#weathericon').setAttribute('src', iconsrc);
     document.querySelector('#weathericon').setAttribute('alt', desc);
