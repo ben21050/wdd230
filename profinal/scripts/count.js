873ae91d9aebdd1ab4253996bd04216f0bf14812
ben21050 <ben21050@byui.edu>
2023-07-02 19:27:19 -0500

    as34

--- profinal/scripts/count.js
+++ profinal/scripts/count.js
@@ -1,27 +1,21 @@
-        // Verificar si existe un contador de bebidas especiales en el almacenamiento local
-        if (localStorage.getItem('specialDrinksCount')) {
-            // Obtener el valor actual del contador y actualizar la tarjeta de información
-            var count = parseInt(localStorage.getItem('specialDrinksCount'));
-            document.getElementById('special-drinks-count').textContent = count;
-        } else {
-            // Si no existe un contador, inicializarlo a 0
-            localStorage.setItem('specialDrinksCount', '0');
-        }
+        // Obtener referencia al formulario
+        const form = document.getElementById('fresh-form');
 
-        // Escuchar el evento de envío del formulario y actualizar el contador de bebidas especiales
-        document.getElementById('fresh-form').addEventListener('submit', function(event) {
-            event.preventDefault(); // Evitar el envío del formulario
+        // Obtener referencia al contador de bebidas especiales
+        const countSpan = document.getElementById('special-drinks-count');
+
+        // Variable para almacenar el contador localmente
+        let specialDrinksCount = 0;
+
+        // Escuchar el evento de envío del formulario
+        form.addEventListener('submit', function(event) {
+            event.preventDefault(); // Evitar el envío del formulario por defecto
 
             // Incrementar el contador de bebidas especiales
-            var count = parseInt(localStorage.getItem('specialDrinksCount'));
-            count++;
-            localStorage.setItem('specialDrinksCount', count.toString());
+            specialDrinksCount++;
 
-            // Actualizar la tarjeta de información
-            document.getElementById('special-drinks-count').textContent = count;
+            // Actualizar el texto del contador en la página
+            countSpan.textContent = specialDrinksCount;
         });
-
-
-
 
 
