0115aac12de3c135ee497c7c5ae3fefb97ad8c6f
ben21050 <ben21050@byui.edu>
2023-07-03 12:44:38 -0500

    uy

--- profinal/scripts/result.js
+++ profinal/scripts/result.js
@@ -1,58 +1,65 @@
-const resultDiv = document.getElementById('result');
 
-// Obtener los valores seleccionados del formulario
-const name = document.getElementById('fname').value;
-const email = document.getElementById('email').value;
-const phone = document.getElementById('phone').value;
-const fruit1 = document.getElementById('fruit1').value;
-const fruit2 = document.getElementById('fruit2').value;
-const fruit3 = document.getElementById('fruit3').value;
-const comments = document.getElementById('comments').value;
+    const form = document.getElementById('fresh-form');
+    const resultDiv = document.getElementById('result');
 
-// Obtener la fecha actual
-const currentDate = new Date().toLocaleDateString();
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
 
-// Obtener los datos de las frutas del archivo JSON
-fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
-  .then(response => response.json())
-  .then(data => {
-    // Obtener los valores de carbohidratos, proteínas, grasas, azúcar y calorías para las frutas seleccionadas
-    const fruitData1 = data.find(fruit => fruit.name === fruit1);
-    const fruitData2 = data.find(fruit => fruit.name === fruit2);
-    const fruitData3 = data.find(fruit => fruit.name === fruit3);
+      const name = form.fname.value;
+      const email = form.email.value;
+      const phone = form.phone.value;
+      const fruit1 = form.fruit1.value;
+      const fruit2 = form.fruit2.value;
+      const fruit3 = form.fruit3.value;
+      const comments = form.comments.value;
 
-    const carbohydrates = fruitData1.carbohydrates + fruitData2.carbohydrates + fruitData3.carbohydrates;
-    const protein = fruitData1.protein + fruitData2.protein + fruitData3.protein;
-    const fat = fruitData1.fat + fruitData2.fat + fruitData3.fat;
-    const calories = fruitData1.calories + fruitData2.calories + fruitData3.calories;
-    const sugar = fruitData1.sugar + fruitData2.sugar + fruitData3.sugar;
+      const currentDate = new Date().toLocaleDateString();
 
-    // Generar el contenido de salida formateado
-    const output = `
-      <h2>Order Summary</h2>
-      <p><strong>Name:</strong> ${name}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Phone:</strong> ${phone}</p>
-      <p><strong>Selected Fruits:</strong></p>
-      <ul>
-        <li>${fruit1}</li>
-        <li>${fruit2}</li>
-        <li>${fruit3}</li>
-      </ul>
-      <p><strong>Additional Comments/Questions:</strong> ${comments}</p>
-      <p><strong>Date:</strong> ${currentDate}</p>
-      <h3>Nutritional Information</h3>
-      <p><strong>Total Carbohydrates:</strong> ${carbohydrates.toFixed(2)}</p>
-      <p><strong>Total Protein:</strong> ${protein.toFixed(2)}</p>
-      <p><strong>Total Fat:</strong> ${fat.toFixed(2)}</p>
-      <p><strong>Total Calories:</strong> ${calories.toFixed(2)}</p>
-      <p><strong>Total Sugar:</strong> ${sugar.toFixed(2)}</p>
-    `;
+      fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
+        .then(response => response.json())
+        .then(data => {
+          const fruitData1 = data.find(fruit => fruit.name === fruit1);
+          const fruitData2 = data.find(fruit => fruit.name === fruit2);
+          const fruitData3 = data.find(fruit => fruit.name === fruit3);
 
-    // Mostrar el resultado en la página
-    resultDiv.innerHTML = output;
-  })
-  .catch(error => {
-    console.log('Error:', error);
-    resultDiv.innerHTML = '<p>An error occurred while fetching fruit data.</p>';
-  });
+          // Verificar si los datos de nutrición de las frutas están disponibles
+          if (!fruitData1?.nutritions || !fruitData2?.nutritions || !fruitData3?.nutritions) {
+            resultDiv.innerHTML = '<p>One or more selected fruits do not have nutrition data.</p>';
+            return;
+          }
+
+          const carbohydrates = fruitData1.nutritions.carbohydrates + fruitData2.nutritions.carbohydrates + fruitData3.nutritions.carbohydrates;
+          const protein = fruitData1.nutritions.protein + fruitData2.nutritions.protein + fruitData3.nutritions.protein;
+          const fat = fruitData1.nutritions.fat + fruitData2.nutritions.fat + fruitData3.nutritions.fat;
+          const calories = fruitData1.nutritions.calories + fruitData2.nutritions.calories + fruitData3.nutritions.calories;
+          const sugar = fruitData1.nutritions.sugar + fruitData2.nutritions.sugar + fruitData3.nutritions.sugar;
+
+          const output = `
+            <h2>Order Details:</h2>
+            <p>Name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Phone: ${phone}</p>
+            <p>Comments: ${comments}</p>
+            <h2>Selected Fruits:</h2>
+            <ul>
+              <li>${fruit1}</li>
+              <li>${fruit2}</li>
+              <li>${fruit3}</li>
+            </ul>
+            <h2>Order Date:</h2>
+            <p>${currentDate}</p>
+            <h2>Nutritional Information:</h2>
+            <p>Carbohydrates: ${carbohydrates} g</p>
+            <p>Protein: ${protein} g</p>
+            <p>Fat: ${fat} g</p>
+            <p>Calories: ${calories}</p>
+            <p>Sugar: ${sugar} g</p>
+          `;
+
+          resultDiv.innerHTML = output;
+        })
+        .catch(error => {
+          console.log('Error:', error);
+          resultDiv.innerHTML = '<p>An error occurred while fetching fruit data.</p>';
+        });
+    });
