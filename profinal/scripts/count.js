6904b03c74919b7ad7d5c03763e7a4c091ffe772
ben21050 <ben21050@byui.edu>
2023-07-02 19:45:49 -0500

    as

--- profinal/scripts/count.js
+++ profinal/scripts/count.js
@@ -1,29 +1,26 @@
-        // Obtener referencia al formulario
-        const form = document.getElementById('fresh-form');
+// Obtener referencia al formulario
+const form = document.getElementById('fresh-form');
 
-        // Obtener referencia al contador de bebidas especiales
-        const countSpan = document.getElementById('special-drinks-count');
+// Obtener referencia al contador de bebidas especiales
+const countSpan = document.getElementById('special-drinks-count');
 
-        // Verificar si hay datos almacenados localmente y actualizar el contador
-        let specialDrinksCount = localStorage.getItem('specialDrinksCount') || 0;
-        countSpan.textContent = specialDrinksCount;
+// Verificar si hay datos almacenados localmente y actualizar el contador
+let specialDrinksCount = localStorage.getItem('specialDrinksCount') || 0;
+countSpan.textContent = specialDrinksCount;
 
-        // Escuchar el evento de envío del formulario
-        form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Evitar el envío del formulario por defecto
+// Escuchar el evento de envío del formulario
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar el envío del formulario por defecto
 
-            // Incrementar el contador de bebidas especiales
-            specialDrinksCount++;
-            
-            // Guardar el contador en el almacenamiento local
-            localStorage.setItem('specialDrinksCount', specialDrinksCount);
+    // Incrementar el contador de bebidas especiales
+    specialDrinksCount++;
+    
+    // Guardar el contador en el almacenamiento local
+    localStorage.setItem('specialDrinksCount', specialDrinksCount);
 
-            // Actualizar el texto del contador en la página
-            countSpan.textContent = specialDrinksCount;
+    // Actualizar el texto del contador en la página
+    countSpan.textContent = specialDrinksCount;
 
-            // Enviar el formulario
-            form.submit();
-        });
-
-
-
+    // Enviar el formulario
+    form.submit();
+});
