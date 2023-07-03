7c89beddac01c99ba7b656f0ceaf73b19525abb0
ben21050 <ben21050@byui.edu>
2023-07-03 13:04:32 -0500

    as

--- profinal/scripts/count.js
+++ profinal/scripts/count.js
@@ -1,26 +1,31 @@
-// Obtener referencia al formulario
+// Obtén una referencia al formulario y al contador en la página de inicio
 const form = document.getElementById('fresh-form');
+const drinkCountSpan = document.getElementById('drink-count');
 
-// Obtener referencia al contador de bebidas especiales
-const countSpan = document.getElementById('special-drinks-count');
+// Verifica si ya existe un valor almacenado para el contador
+let drinkCount = localStorage.getItem('drinkCount');
+if (!drinkCount) {
+  // Si no hay un valor almacenado, inicializa el contador en 0
+  drinkCount = 0;
+} else {
+  // Si hay un valor almacenado, actualiza el contenido del contador en la página de inicio
+  drinkCountSpan.textContent = drinkCount;
+}
 
-// Verificar si hay datos almacenados localmente y actualizar el contador
-let specialDrinksCount = localStorage.getItem('specialDrinksCount') || 0;
-countSpan.textContent = specialDrinksCount;
-
-// Escuchar el evento de envío del formulario
+// Agrega un controlador de eventos para el envío del formulario
 form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar el envío del formulario por defecto
+  event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
 
-    // Incrementar el contador de bebidas especiales
-    specialDrinksCount++;
-    
-    // Guardar el contador en el almacenamiento local
-    localStorage.setItem('specialDrinksCount', specialDrinksCount);
+  // Incrementa el contador de bebidas
+  drinkCount++;
+  
+  // Almacena el nuevo valor del contador en el almacenamiento local del navegador
+  localStorage.setItem('drinkCount', drinkCount);
 
-    // Actualizar el texto del contador en la página
-    countSpan.textContent = specialDrinksCount;
-
-    // Enviar el formulario
-    form.submit();
+  // Actualiza el contenido del contador en la página de inicio y en la página de formulario
+  drinkCountSpan.textContent = drinkCount;
+  document.getElementById('drink-count-form').textContent = drinkCount;
+  
+  // Restablece los valores del formulario
+  form.reset();
 });
